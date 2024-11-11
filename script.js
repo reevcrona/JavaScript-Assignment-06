e56337b1d096c85652ed27928f54bd2d00a4f692
reevcrona <89569507+reevcrona@users.noreply.github.com>
2024-11-11 13:42:19 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+// Variables for the DOM elements
+
+// Array
+const words = [
+  "dependent",
+  "dog",
+  "superficial",
+  "admit",
+  "juice",
+  "javascript",
+  "developer",
+  "airplane",
+  "great",
+  "fun",
+  "manipulate",
+  "cat",
+  "transition",
+  "school",
+  "computer",
+  "programming",
+  "drag",
+  "loving",
+  "north",
+];
