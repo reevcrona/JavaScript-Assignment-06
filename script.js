49ec6fea6ddc22f8eb7dfac74a585794daa8c133
reevcrona <89569507+reevcrona@users.noreply.github.com>
2024-11-11 14:05:47 +0100

    added timer function that counts down and function that displays a random word from the words array

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 // Variables for the DOM elements
 
+  const displayWord = document.querySelector("#word");
+  const scoreSpan = document.querySelector("#score");
+  const timeSpan = document.querySelector("#time");
+  
+  
+
 // Array
 const words = [
   "dependent",
@@ -22,3 +28,27 @@ const words = [
   "loving",
   "north",
 ];
+
+// Variables
+
+let timer = 10;
+const timeInterval = setInterval(updateTime,1000);
+
+// Functions
+function addWordToDOM(arr){
+    const randomIndex = Math.floor(Math.random() * arr.length);
+
+    const randomWord = arr[randomIndex];
+
+    displayWord.textContent = randomWord
+}
+
+function updateTime(){
+    timer--;
+    timeSpan.textContent = `${timer}s` 
+    if (timer <= 0){
+      console.log("game over");
+      clearInterval(timeInterval)
+    }
+}
+
